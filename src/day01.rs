68852abeda6b61e8b93c9c2d43970df0db895307
Alex van de Sandt <alexander.vandesandt@honeywell.com>
2021-12-01 12:58:31 -0500

    Day 01

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,19 @@
 pub fn part1(input: String) {
-    println!("{}", input.chars().rev().collect::<String>());
+    println!("{}", count_increments(&parse(&input)));
+}
+
+pub fn part2(input: String) {
+    let windows: Vec<_> = parse(&input)
+        .windows(3)
+        .map(|slice| slice.iter().sum())
+        .collect();
+    println!("{}", count_increments(&windows));
+}
+
+fn parse(input: &str) -> Vec<u32> {
+    input.lines().map(|s| s.parse().unwrap()).collect()
+}
+
+fn count_increments(nums: &[u32]) -> usize {
+    nums.windows(2).filter(|slice| slice[0] < slice[1]).count()
 }
